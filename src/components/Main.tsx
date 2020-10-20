445124f100e3d1660df5d485b499a7193fc556ac
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-20 16:11:51 +0300

    made main component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -10,10 +10,7 @@ import { ProfileData } from "./ProfileData";
 import { ProfileForm } from "./ProfileForm";
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		flexGrow: 1,
-	},
-	content: {
+	contentValue: {
 		display: "flex",
 		flexDirection: "column",
 		padding: "0px 23px 0px 34px",
@@ -43,14 +40,43 @@ const useStyles = makeStyles((theme) => ({
 	breadcrumbs__separator: {
 		margin: 0,
 	},
+	["@media (max-width: 684px)"]: {
+		contentValue: {
+			padding: "0px 10px",
+		},
+	},
+	["@media (max-width: 414px)"]: {
+		content__header: {
+			textAlign: "left",
+			fontSize: "14px",
+			lineHeight: "19px",
+			textTransform: "uppercase",
+			fontWeight: 600,
+			width: "100%",
+		},
+		breadcrumbs: {
+			marginTop: "10px",
+			color: "#fff",
+		},
+		breadcrumbs__crumb: {
+			fontSize: "12px",
+			lineHeight: "16px",
+		},
+	},
 }));
 
 export const Content: React.FC<{}> = () => {
 	const styles = useStyles();
-	const [editing, setEditing] = useState(true);
+	const [editing, setEditing] = useState(false);
+	function openEdit() {
+		setEditing(true);
+	}
+	function closeEdit() {
+		setEditing(false);
+	}
 	return (
 		<>
-			<Container className={styles.content}>
+			<Container className={styles.contentValue}>
 				<Typography className={styles.content__header} variant="h1">
 					Личный кабинет
 				</Typography>
@@ -64,7 +90,11 @@ export const Content: React.FC<{}> = () => {
 						Личный кабинет
 					</Typography>
 				</Breadcrumbs>
-				<ProfileSheet />
+				<ProfileSheet
+					openEdit={openEdit}
+					opened={editing}
+					closeEdit={closeEdit}
+				/>
 				{!editing && <ProfileData />}
 				{editing && <ProfileForm />}
 			</Container>
