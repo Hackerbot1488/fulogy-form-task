ba4973c3ac1925d6313198a505d7c9b3ae2667c9
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-21 08:47:00 +0300

    made modal body dynamic

--- src/components/ProfileEditModal/ModalBody.tsx
+++ src/components/ProfileEditModal/ModalBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { ModalBodyControls } from "./ModalBodyControls";
 import classes from "./ModalBody.module.sass";
 import { Button, makeStyles, Typography } from "@material-ui/core";
@@ -13,6 +13,9 @@ const useStyles = makeStyles((theme) => ({
 		lineHeight: "33px",
 		fontWeight: 600,
 		textAlign: "center",
+		marginTop: "84px",
+	},
+	modalBody__header_success: {
 		marginTop: "84px",
 	},
 	modalBody__button_confirm: {
@@ -32,21 +35,50 @@ const useStyles = makeStyles((theme) => ({
 			backgroundColor: "rgba(1, 189, 167, 0.7)",
 		},
 	},
+	["@media (max-width: 414px)"]: {
+		modalBody__header_success: {
+			marginTop: "29px",
+			fontSize: "18px",
+			lineHeight: "25px",
+		},
+	},
 }));
 export const ModalBody: React.FC<ModalBodyProps> = ({ closeModal }) => {
 	const [confirmed, setConfirmed] = useState(false);
+	const [up, setUp] = useState(false);
 	const styles = useStyles();
+	const ref = useRef(false);
 	function confirm() {
 		setConfirmed(true);
 	}
+	let timeout;
+	useEffect(() => {
+		if (!ref.current) {
+			ref.current = true;
+			timeout = setTimeout(() => {
+				setUp(true);
+			}, 0);
+		}
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, [timeout]);
 	return (
-		<div className={classes.modalBody}>
+		<div
+			className={classes.modalBody + " " + (up ? classes.modalBody_up : "")}
+			id="modal-body"
+		>
 			{!confirmed && (
 				<ModalBodyControls closeModal={closeModal} confirm={confirm} />
 			)}
 			{confirmed && (
 				<div className={classes.modalBody__success}>
-					<Typography variant="h2" className={styles.modalBody__header}>
+					<Typography
+						variant="h2"
+						className={
+							styles.modalBody__header + " " + styles.modalBody__header_success
+						}
+					>
 						Данные успешно сохранены.
 					</Typography>
 					<Button
