d40bbd427f8dd5599132f7fd07b4cd1212e6d3dd
Hackerbot1488 <gromovartem04@mail.ru>
2020-10-21 12:44:12 +0300

    made structure for profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Header } from "./Header";
-import { Content } from "./Main";
+import { Header } from "./Header/Header";
+import { Content } from "./Content/Main";
 
 export const Profile: React.FC<{}> = () => {
 	return (
